61ed4886344f1a4849b314d543bc50d0d57feb31
Rajat Popli <rpopli5@gmail.com>
2021-07-13 17:08:46 +0530

    First version

---
+++ wiki-styled-editing.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Plugin Name: Wiki Styled Editing
+* Plugin URI: 
+* Description: An easy way for the subscriber of your website to modify the blog posts.
+* Version: 1.0.0
+* Licence: GPL V2 or later
+* Author: Rajat Popli
+* Author URI: https://www.linkedin.com/in/rajat-popli-553472135/
+* Text Domain: wse
+*/
+
+// Includes
+include ('includes/admin-page.php');
+include ('includes/display-functions.php');
